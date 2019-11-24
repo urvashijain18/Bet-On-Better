b8ae4d28f8d74b8b7285b6fd7b984b0b52ee2e38
unknown <urvashijain003@gmail.com>
2019-11-24 17:07:17 -0500

    New JPanels

--- Bet_On_Better/src/UserInterface/Event/CreateEventJPanel.java
+++ Bet_On_Better/src/UserInterface/Event/CreateEventJPanel.java
@@ -3,18 +3,18 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package UserInterface.DonorRole;
+package UserInterface.Event;
 
 /**
  *
  * @author @author Urvashi
  */
-public class DonorWorkAreaJPanel extends javax.swing.JPanel {
+public class CreateEventJPanel extends javax.swing.JPanel {
 
     /**
-     * Creates new form DonorWorkAreaJPanel
+     * Creates new form CreateEventJPanel
      */
-    public DonorWorkAreaJPanel() {
+    public CreateEventJPanel() {
         initComponents();
     }
 
