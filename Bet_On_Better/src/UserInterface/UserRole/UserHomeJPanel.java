e7517fbf26332f2dcc2df5fb42ceb82ef26b79ee
ANUSHA TALASILA <talasila.a@husky.neu.edu>
2019-11-26 01:49:12 -0500

    UserAccDirec

--- Bet_On_Better/src/UserInterface/UserRole/UserHomeJPanel.java
+++ Bet_On_Better/src/UserInterface/UserRole/UserHomeJPanel.java
@@ -20,19 +20,17 @@ import javax.swing.JPanel;
 public class UserHomeJPanel extends javax.swing.JPanel {
   private JPanel leftContainer;
     private JPanel rightContainer;
-    private UserAccountDirectory userAccountDirectory; 
     private Enterprise enterprise; 
     private EventDirectory eventdirectory;
         
     /**
      * Creates new form UserHomeJPanel
      */
-    public UserHomeJPanel(JPanel leftContainer, JPanel rightContainer,UserAccountDirectory userAccountDirectory,Enterprise enterprise,EventDirectory eventdirectory) {
+    public UserHomeJPanel(JPanel leftContainer, JPanel rightContainer, Enterprise enterprise,EventDirectory eventdirectory) {
         initComponents();
          this.leftContainer = leftContainer;
         this.rightContainer = rightContainer;
         this.enterprise = enterprise;
-        this.userAccountDirectory = userAccountDirectory;
         this.eventdirectory = eventdirectory;
     }
 
@@ -166,12 +164,10 @@ public class UserHomeJPanel extends javax.swing.JPanel {
         // TODO add your handling code here:
         rightContainer.remove(this);
         CardLayout rightCardLayout = (CardLayout) rightContainer.getLayout();
-        rightContainer.add("DonateJPanel", new DonateJPanel(leftContainer, rightContainer, userAccountDirectory, enterprise, eventdirectory));
+        rightContainer.add("DonateJPanel", new DonateJPanel(leftContainer, rightContainer, enterprise, eventdirectory));
         rightCardLayout.next(rightContainer);
-        Component [] components = rightContainer.getComponents();
-        for(Component c : components){
+       
         
-        }
     }//GEN-LAST:event_btnDonateActionPerformed
 
 
